adb9ff6ebcf5c453e90edbdc02602dab22c25a75
Patrick Ngabonziza <ngabonzizap7@gmail.com>
2019-06-18 11:16:54 +0200

    bg-adding router on update ft

---
+++ server/routers/router.js
@@ -0,0 +1,9 @@
+import express from "express";
+import Cloths from "../controllers/controller";
+
+const router = express.Router();
+
+router.post('/api/v1/cloths', Cloths.CreatItem);
+router.patch('/api/v1/cloths/:id',Cloths.updateItem);
+
+export default router;
