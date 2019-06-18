d0491ce805e0ebb250cc88ff936279c5068356e7
kagabof <faustinkagabo@gmail.com>
2019-06-18 09:46:34 +0200

    fix test

--- server/test/test.js
+++ server/test/test.js
@@ -9,15 +9,15 @@ describe("Cloths", () => {
     describe("POST/", () =>{
         it("should create an Item in the stock", (done) => {
             const item = {
-                "name":"pants",
+                "name":"tshit",
                 "price": 5000,
                 "description": "black pants"
             };
             chai.request(app)
                 .post(`/api/v1/cloths`)
-                .send(user)
+                .send(item)
                 .end((req, res) => {
-                    res.should.have.status(200);
+                    res.should.have.status(201);
                     res.body.should.be.a('object');
                     done();
                 });
